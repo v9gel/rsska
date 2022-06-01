d8a6d5b76c3fe24f8fe38632d3c405263372c876
Vyacheslav <v9gel@mail.ru>
2022-06-01 22:49:39 +0700

    + линтер

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx', '.ts'] }],
+    'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'warn',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
